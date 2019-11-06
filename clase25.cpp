721993c24c80784cee454f6118762b0d639eb056
laums25 <lm.mercado@uniandes.edu.co>
2019-11-06 23:43:40 +0000

    clase

--- clase25.cpp
+++ clase25.cpp
@@ -1,58 +1,29 @@
 #include <iostream>
-#include <cmath>
 
-int N=0;
-float Matriz[0][0];
+int valor=0;
 
-void relleno(int N,float **Matriz);
+void relleno(int valor, float **f);
 
-void imprimecabezas(int N,float **Matriz);
+void imprimeCabezas(int valor, float **f);
 
-void imprime(int N,float **Matriz);
-
-
-void relleno(int N,float **Matriz){
-    for (int i=0; i<N+2; i++){
-        for (int j=0; j<N; j++){
-            
-        
-            Matriz[i][j]=i+j;
-            std::cout<< Matriz[i][j]<< std::endl;
-            
-    }  
-}
-      
-}
-
-void imprimecabezas(int N, float **Matriz){
-
-    float sum=0;
-
-    for (int i =0; i <N;i++) {
-
-    std::cout<<Matriz[i][0]<<" ";
-
-    sum+=Matriz[i][0];
-
-
-    }
-}
+void obtenerM(int valor, float **f);
 
 
 
 int main(void){
     
     std::cout<<"Escriba un numero del 3 al 11 "<<std::endl;
-    std::cin>>N;
     
-    if (N>3 && N<11){
-       new float *[N];
-       for (int i=0; i<N; i++){
-           Matriz[i] = new float[N+2];
+    std::cin>>valor;
+    
+    if (valor>=3 && valor<=11){
+       float ** Matriz = new float *[valor];
+       for (int i=0; i<valor; i++){
+           Matriz[i] = new float[valor+2];
        }
-       std::cout<<relleno(N, Matriz)<<std::endl;
-       std::cout<<imprimecabezas(N,Matriz)<<std::cout;
-    
+       relleno(valor, Matriz);
+       imprimeCabezas(valor,Matriz);
+       obtenerM(valor,Matriz);
     }
     
     else{
@@ -64,6 +35,58 @@ int main(void){
     return 0;
     
     
+}
+
+
+void relleno(int valor,float **f){
+    for (int i=0; i<valor+2; i++){
+        for (int j=0; j<valor; j++){
+            
+
+            f[j][i]=i+j;
+            
+    }  
+}
+      
+}
+
+void imprimeCabezas(int valor, float **f){
+
+    float sumatoria=0;
+	float sumar=0;
+    
+	std::cout<<"Primera fila: "<<" ";
+    for (int i =0; i <valor;i++) {
+	std::cout<<f[i][0]<<" ";
+	sumatoria+=f[i][0];
+    }
+    
+    std::cout<<std::endl;
+	
+	for (int j=0; j<valor+2;j++){
+		sumar+=f[0][j];
+	}
+	std::cout<<std::endl;
+    std::cout<<"Suma de los elementos de la primera columna: "<<" "<<sumar<<std::endl;
+
+}
+
+
+void obtenerM(int valor,float **f){
+
+  for (int  j =0;  j < valor+2; j++) {
+
+
+      for (int i = 0; i <valor; i++) {
+
+        std::cout<<"     "<<f[i][j]<<"     ";
+
+  }
+
+  std::cout<<std::endl;
+
+  }
+
 }
 
 
